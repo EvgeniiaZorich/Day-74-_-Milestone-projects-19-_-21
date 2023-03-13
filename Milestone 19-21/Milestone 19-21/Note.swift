a93a75527141b07a2685a33ef71a9fb33dcddde8
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-03-13 19:39:15 +0100

    initial commit

---
+++ Milestone 19-21/Milestone 19-21/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Milestone 19-21
+//
+//  Created by Евгения Зорич on 13.03.2023.
+//
+
+import Foundation
---
+++ Milestone 19-21/Milestone 19-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone 19-21
+//
+//  Created by Евгения Зорич on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
